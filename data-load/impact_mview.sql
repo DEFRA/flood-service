9533c32a857a63cfcbec4ded75462d1d9cb582be
tedd mason <teddmason@gmail.com>
2019-06-11 14:33:25 +0100

    Only calculating ffoi risk if is an ffoi station

--- data-load/impact_mview.sql
+++ data-load/impact_mview.sql
@@ -23,11 +23,12 @@ i.source,
 s.processed_value as telemetryLatest,
 s.processed_value >= i.value as telemetryActive,
 ffoi.value as forecastMax,
-ffoi.value >= i.value as forecastActive
+(ffoi.value >= i.value AND fs.rloi_id is not null) as forecastActive
 from u_flood.impact i
 inner join u_flood.telemetry_context tc on i.rloi_id = tc.rloi_id
 left join u_flood.stations_overview_mview s on s.rloi_id = i.rloi_id and s.direction = 'u'
 left join u_flood.ffoi_max ffoi on ffoi.telemetry_id = s.telemetry_id
+left join u_flood.ffoi_station fs on i.rloi_id = fs.rloi_id
 
 WITH DATA;
 
