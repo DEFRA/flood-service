0b2a4e4db4ef6a7eff1ae1accccf77b476e84a8c
tedd mason <teddmason@gmail.com>
2019-08-06 10:12:56 +0100

    config test converage

---
+++ test/config.js
@@ -0,0 +1,21 @@
+const Lab = require('@hapi/lab')
+const lab = exports.lab = Lab.script()
+const Code = require('code')
+let connectionString
+
+lab.experiment('Config test', () => {
+  lab.beforeEach(() => {
+    connectionString = process.env.FLOOD_SERVICE_CONNECTION_STRING
+    delete process.env.FLOOD_SERVICE_CONNECTION_STRING
+    delete require.cache[require.resolve('../server/config')]
+  })
+
+  lab.afterEach(() => {
+    process.env.FLOOD_SERVICE_CONNECTION_STRING = connectionString
+    delete require.cache[require.resolve('../server/config')]
+  })
+
+  lab.test('Check bad config fails', () => {
+    Code.expect(() => { require('../server/config') }).to.throw()
+  })
+})
