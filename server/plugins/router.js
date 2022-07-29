a0efe51aa5eb59cd4f9141ebdd5980a416a70e59
LeeGordon83 <lee.gordon83@gmail.com>
2022-07-29 15:34:39 +0100

    added river by name route

--- server/plugins/router.js
+++ server/plugins/router.js
@@ -27,7 +27,9 @@ const routes = [].concat(
   require('../routes/stations-by-radius'),
   require('../routes/error'),
   require('../routes/rainfall-station-telemetry'),
-  require('../routes/river-name')
+  require('../routes/river-name'),
+  require('')
+
 )
 
 module.exports = {
