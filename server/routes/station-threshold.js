8c882d805403e01a0c1813d1a84a467695cbc093
John Shields <John.Shields@environment-agency.gov.uk>
2022-09-09 16:58:27 +0100

    FSR-595: Add station threshold route and db query.

---
+++ server/routes/station-threshold.js
@@ -0,0 +1,49 @@
+const joi = require('@hapi/joi')
+const floodsService = require('../services/index')
+
+module.exports = {
+  method: 'GET',
+  path: '/station/{id}/threshold',
+  handler: async request => {
+    try {
+      const stationThreshold = await floodsService.getStationThreshold(request.params.id)
+      if (stationThreshold.length > 0) {
+        const warningAreasThresholds = []
+        const alertAreasThresholds = []
+
+        stationThreshold.forEach(element => {
+          if (element.fwis_code.charAt(4).toLowerCase() === 'w') {
+            const warningAreaThreshold = {}
+            warningAreaThreshold.floodWarningArea = element.fwis_code
+            warningAreaThreshold.level = element.value
+            warningAreasThresholds.push(warningAreaThreshold)
+          } else {
+            const alertAreaThreshold = {}
+            alertAreaThreshold.floodWarningArea = element.fwis_code
+            alertAreaThreshold.level = element.value
+            alertAreasThresholds.push(alertAreaThreshold)
+          }
+        })
+
+        // Return alert and warning threshold
+
+        const alertMin = Math.min(...alertAreasThresholds.map(item => item.level))
+        const warningMin = Math.min(...warningAreasThresholds.map(item => item.level))
+
+        return [warningMin, alertMin]
+      } else {
+        return [null, null]
+      }
+    } catch (err) {
+      return { error: `Failed to get station threshold data: ${err}` }
+    }
+  },
+  options: {
+    description: 'Get station threshold by station id',
+    validate: {
+      params: joi.object({
+        id: joi.number().required()
+      })
+    }
+  }
+}
