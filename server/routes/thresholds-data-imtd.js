51826740dd685c069b05962a3b7780b5b80e2d74
John Shields <John.Shields@environment-agency.gov.uk>
2022-09-11 17:30:53 +0100

    FSR-595: Add unit test and remove unused collections.

--- server/routes/thresholds-data-imtd.js
+++ server/routes/thresholds-data-imtd.js
@@ -1,6 +1,8 @@
 const joi = require('@hapi/joi')
 const util = require('../util')
 
+// This module retieves station thresholds directly from the IMFS Thresholds API. This route was created for initial prototyping purposes and has been
+// replaced by a new route /station/{rloi_id}/threshold.
 module.exports = {
   method: 'GET',
   path: '/station/{id}/imtd/thresholds',
@@ -9,22 +11,18 @@ module.exports = {
       const imtdUrl = `https://imfs-prd1-thresholds-api.azurewebsites.net/Location/${request.params.id}?version=2`
       const imtdPayload = await util.getJson(imtdUrl)
 
-      const warningThresholds = []
       const warningAreasThresholds = []
-      const alertThresholds = []
       const alertAreasThresholds = []
       imtdPayload[0].TimeSeriesMetaData.forEach(element => {
         element.Thresholds.forEach(threshold => {
           if (threshold.ThresholdType === 'FW ACT FW' || threshold.ThresholdType === 'FW ACTCON FW' || threshold.ThresholdType === 'FW RES FW') {
             const warningAreaThreshold = {}
-            warningThresholds.push(threshold.Level)
             warningAreaThreshold.floodWarningArea = threshold.FloodWarningArea
             warningAreaThreshold.level = threshold.Level
             warningAreasThresholds.push(warningAreaThreshold)
           }
           if (threshold.ThresholdType === 'FW ACT FAL' || threshold.ThresholdType === 'FW ACTCON FAL' || threshold.ThresholdType === 'FW RES FAL') {
             const alertAreaThreshold = {}
-            alertThresholds.push(threshold.Level)
             alertAreaThreshold.floodWarningArea = threshold.FloodWarningArea
             alertAreaThreshold.level = threshold.Level
             alertAreasThresholds.push(alertAreaThreshold)
